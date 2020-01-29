bde1616fdf627c33b18028ef95574e20363b30b0
Breno Nesto <breno.nesto@hotmail.com>
2020-01-29 18:17:11 -0300

    primeiro desafio do fastfeet, autenticacao e crud

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '06a6474630411e37ba2dd0c0a9f089e1',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/recipients', authMiddleware, RecipientController.store);
+
+routes.put('/recipients', authMiddleware, RecipientController.update);
+
+export default routes;
